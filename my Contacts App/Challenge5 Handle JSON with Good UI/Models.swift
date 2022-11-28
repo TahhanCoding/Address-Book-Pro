73141697b1473ed8448e8352117a1c8977988039
Ahmed Sha'ban <a.sh.tahan@gmail.com>
2022-11-28 06:07:27 +0200

    first commit using personal access token; added a comment line in Models files arranged files of the project

--- my Contacts App/Challenge5 Handle JSON with Good UI/Models.swift
+++ my Contacts App/Challenge5 Handle JSON with Good UI/Models.swift
@@ -2,6 +2,7 @@
 //  Challenge5 Handle JSON with Good UI
 //  Created by Ahmed Shaban on 20/08/2022.
 
+// This is first real commit try using git in sourceTree, auth by classic personal access token that has full credentials, the token is used instead of password to auth. over https
 import Foundation
 
 struct Result: Codable {
